1acdd1384318b5574d461914b185c8ecc0a6a5ca
Der_Floh <derr.floh@gmail.com>
2024-04-30 13:56:07 +0200

    first commit

---
+++ docs/scripts/main.js
@@ -0,0 +1,83 @@
+let devInput;
+let devPufferInput;
+let devCheckInput;
+let devTestInput;
+let devDocInput;
+let custProvInput;
+let custDelInput;
+let custGarInput;
+let resultInput;
+let lockCalc = false;
+
+document.addEventListener("DOMContentLoaded", () => {
+    devInput = document.getElementById("dev-input");
+    devPufferInput = document.getElementById("dev-puffer-input");
+    devCheckInput = document.getElementById("dev-check-input");
+    devTestInput = document.getElementById("dev-test-input");
+    devDocInput = document.getElementById("dev-doc-input");
+    custProvInput = document.getElementById("cust-prov-input");
+    custDelInput = document.getElementById("cust-del-input");
+    custGarInput = document.getElementById("cust-gar-input");
+    resultInput = document.getElementById("result-input");
+});
+
+function onInputDev(event) {
+    if (lockCalc === false)
+        updateCorrInputs();
+    calcResult();
+}
+
+function onInputDevPuffer(event) {
+    devPufferChanged = event.target.value !== 0;
+    calcResult();
+}
+
+function onInputDevCheck(event) {
+    devPufferChanged = event.target.value !== 0;
+    calcResult();
+}
+
+function onInputDevTest(event) {
+    devPufferChanged = event.target.value !== 0;
+    calcResult();
+}
+
+function onInputDevDoc(event) {
+    devPufferChanged = event.target.value !== 0;
+    calcResult();
+}
+
+function onInputCustProv(event) {
+    devPufferChanged = event.target.value !== 0;
+    calcResult();
+}
+
+function onInputCustDel(event) {
+    devPufferChanged = event.target.value !== 0;
+    calcResult();
+}
+
+function onInputCustGar(event) {
+    devPufferChanged = event.target.value !== 0;
+    calcResult();
+}
+
+function onInputLockCalc(event) {
+    lockCalc = !event.target.checked;
+}
+
+function onClickCalcButton() {
+    updateCorrInputs();
+    calcResult();
+}
+
+function updateCorrInputs() {
+    devCheckInput.value = ((+devInput.value) * 0.1).toFixed(2);
+    devTestInput.value = ((+devInput.value) * 0.1).toFixed(2);
+    custProvInput.value = ((+devInput.value) * 0.05).toFixed(2);
+}
+
+function calcResult() {
+    const result = (+devInput.value) + (+devPufferInput.value) + (+devCheckInput.value) + (+devTestInput.value) + (+devDocInput.value) + (+custProvInput.value) + (+custDelInput.value) + (+custGarInput.value);
+    resultInput.value = result.toFixed(2);
+}
